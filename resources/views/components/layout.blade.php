f230e10939a32b681b85697b9425c67218133fe6
appie1312 <agoeswag@gmil.com>
2025-09-19 14:18:39 +0200

    opdracht 5 af

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,22 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full bg-gray-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>home</title>
       <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body>
-   <!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> -->
-<!--
-  This example requires updating your template:
+<body class="h-full">
+   
 
-  ```
-  <html class="h-full bg-gray-900">
-  <body class="h-full">
-  ```
--->
 <div class="min-h-full">
   <nav class="bg-gray-800/50">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -28,9 +20,9 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-              <a href="/" aria-current="page" class="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white">home</a>
-              <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">about</a>
-              <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">contact</a>
+              <x-nav-link href="/"  :active="request()->is('/')">home</x-nav-link>
+              <x-nav-link href="/about" :active="request()->is('about')">about</x-nav-link>
+              <x-nav-link href="/contact" :active="request()->is('contact')"  type="button">contact</x-nav-link>
             </div>
           </div>
         </div>
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,1 +1,12 @@
-    <a {{ $attributes }}>{{ $slot }}</a>
+@props (['active' => false])
+
+<?php if ($type === 'a') ?>
+
+
+<a  aria-current="page" class="{{ $active? 'bg-gray-950/50 text-white rounded-md px-3 py-2 text-sm font-medium': 'text-gray-300 hover:bg-white/5 hover:text-white rounded-md px-3 py-2 text-sm font-medium'}}"{{$attributes}}>{{$slot}}</a>
+
+<?php else : ?>
+    <button {{ $attributes }}>
+        {{ $slot }}
+    </button>
+<?php @endif ?>
