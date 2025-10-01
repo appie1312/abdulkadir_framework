dc223a337514c73b6a4157c663822c77426ee63c
appie1312 <agoeswag@gmil.com>
2025-10-01 10:32:38 +0200

    opdr 7 af

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,22 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Models\job;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
+Route::get('/jobs', function ()  {
     return view('jobs', [
-        'jobs' => [
-            ['id' => 1, 'title' => 'Director', 'salary' => 50000],
-            ['id' => 2, 'title' => 'Programmer', 'salary' => 10000],
-            ['id' => 3, 'title' => 'Teacher', 'salary' => 40000],
-        ],
+        'jobs' => Job::all()
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        ['id' => 1, 'title' => 'Director', 'salary' => 50000],
-        ['id' => 2, 'title' => 'Programmer', 'salary' => 10000],
-        ['id' => 3, 'title' => 'Teacher', 'salary' => 40000],
-    ];
+Route::get('/jobs/{id}', function ($id)  {
 
-    $job = collect($jobs)->first(fn ($job) => $job['id'] == $id);
+    $job = Job::find($id);
 
-    if (!$job) {
-        abort(404);
-    }
+ 
 
     return view('job', ['job' => $job]);
 }); 
